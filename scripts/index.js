b0b1a66d183d33e14951d16dcd78082eea1a8284
58183 <58183@etu.he2b.be>
2024-09-08 16:09:36 +0200

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,47 @@
+
+// Do the intro animation
+var typed = new Typed(".multiple-text", {
+    strings: ["Software Developer", "Mainly Backend Dev", "OO Developer"], typeSpeed: 100, backSpeed: 100, backDelay: 1000, loop: true
+})
+
+// Do the projects scroll
+const projectsCases = document.querySelector('.projects-cases');
+const arrowLeft = document.querySelector('.arrow-left');
+const arrowRight = document.querySelector('.arrow-right');
+const scrollAmount = 350+60;
+
+// Define how divs move with each click on arrows
+arrowLeft.addEventListener('click', function() {
+projectsCases.scrollBy({
+left: -scrollAmount,
+behavior: 'smooth'
+});
+});
+
+arrowRight.addEventListener('click', function() {
+projectsCases.scrollBy({
+left: scrollAmount,
+behavior: 'smooth'
+});
+});
+
+
+//Do the launcher video
+const overlayBox = document.querySelector('.overlay-box');
+const videoPlayer = document.querySelector('.video-player');
+const imageContainers = document.querySelectorAll('.image-container');
+
+imageContainers.forEach((container) => {
+const overlayBox = container.querySelector('.overlay-box');
+const videoPlayer = container.querySelector('.video-player');
+
+overlayBox.addEventListener('mouseenter', () => {
+videoPlayer.style.display = 'block';
+videoPlayer.play();
+});
+
+overlayBox.addEventListener('mouseleave', () => {
+videoPlayer.style.display = 'none';
+videoPlayer.pause();
+});
+});
